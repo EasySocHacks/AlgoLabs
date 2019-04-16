1bf124e92729602083ce688247f8c32069b4be5c
Никита Елфимов <47705038+EasySocHacks@users.noreply.github.com>
2019-04-17 01:54:19 +0300

    Create C.cpp

---
+++ Lab_1/C.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+const int LEN = 500000;
+
+int n;
+int arr[LEN];
+long long ans = 0;
+
+void mergeSort(int l, int r) {
+    if (r <= l)
+        return;
+
+    int newArr[r - l + 1];
+
+    mergeSort(l, (r + l) / 2);
+    mergeSort((r + l) / 2 + 1, r);
+
+    int first = l;
+    int second = (r + l) / 2 + 1;
+
+    while (first <= (r + l) / 2 || second <= r) {
+        if (first <= (r + l) / 2 && second <= r) {
+            if (arr[first] <= arr[second]) {
+                newArr[first - l + second - ((r + l) / 2 + 1)] = arr[first];
+                first++;
+            } else {
+                ans += ((r + l) / 2 - first + 1);
+
+                newArr[first - l + second - ((r + l) / 2 + 1)] = arr[second];
+                second++;
+            }
+        } else if (first <= (r + l) / 2) {
+            newArr[first - l + second - ((r + l) / 2 + 1)] = arr[first];
+            first++;
+        } else {
+            newArr[first - l + second - ((r + l) / 2 + 1)] = arr[second];
+            second++;
+        }
+    }
+
+    for (int i = l; i <= r; i++) {
+        arr[i] = newArr[i - l];
+    }
+}
+
+int main() {
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    mergeSort(0, n - 1);
+
+    printf("%lld", ans);
+
+    return 0;
+}
